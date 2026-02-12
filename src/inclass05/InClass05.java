53ed2a1b6f514a9495fb7f3c6accf13c2f63b4d7
Ishanpreet-Singh <sin14799@sheridancollege.ca>
2026-02-12 17:52:33 -0500

    Add a yml file

---
+++ src/inclass05/InClass05.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inclass05;
+
+/**
+ *
+ * @author ishan
+ */
+public class InClass05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
